3c9f656a4ac6ddb28c31e3dfb7a8c9f814a2fbdc
Michael Terrefortes <60881801+michaelterrefortes@users.noreply.github.com>
2021-05-16 23:24:29 -0400

    Update Proj2.java

--- Proj2.java
+++ Proj2.java
@@ -2,7 +2,9 @@
 Project 2
 801-18-9110
 CCOM4029-001 Prof. Patricia Ordonez
-This program is a Rummy game with GUI*/
+This program is a Rummy game with GUI
+This is the file that needs to be run in order to start the game
+*/
 
 public class Proj2{
     public static void main(String args[])
